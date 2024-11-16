b04520bd20b8de3d0c2d0459ad6b3a65455b9698
tammarabm <165035795+tammarabm@users.noreply.github.com>
2024-11-15 23:51:48 -0300

    update main

--- proyecto/src/Main.java
+++ proyecto/src/Main.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		GestionGuardia gestionGuardia = new GestionGuardia();
 		Random random = new Random();
+		GestionGuardia gestionGuardia = new GestionGuardia();
 
 	while (true) {
 	    System.out.println("\n--- Menú de Operaciones ---");
@@ -18,33 +18,27 @@ public class Main {
 	
 	    switch (opcion) {
 	        case 1:
-	            System.out.print("Ingrese el código del medicamento: ");
-	            int codigo = input.nextInt();
-	            input.nextLine();
-	            System.out.print("Ingrese la descripción del medicamento: ");
-	            String descripcion = input.nextLine();
-	            System.out.print("Ingrese el precio unitario: ");
-	            double precio = input.nextDouble();
-	            System.out.print("Ingrese el stock disponible: ");
-	            int stock = input.nextInt();
-	            input.nextLine();
-	            Medicamento medicamento= new Medicamento(codigo, descripcion, precio, stock);
-	            gestionGuardia.registrarMedicamento(medicamento);
-	            System.out.println("Medicamento registrado con éxito.");
+	        	System.out.print("Ingrese la cantidad de medicamentos que puede registrar el hospital: ");
+	            int capacidad = input.nextInt();
+	            
+	        	for (int i = 1; i <= capacidad; i++) {
+		            System.out.print("Ingrese el código del medicamento: ");
+		            int codigo = input.nextInt();
+		            input.nextLine();
+		            System.out.print("Ingrese la descripción del medicamento: ");
+		            String descripcion = input.nextLine();
+		            System.out.print("Ingrese el precio unitario: ");
+		            double precio = input.nextDouble();
+		            System.out.print("Ingrese el stock disponible: ");
+		            int stock = input.nextInt();
+		            input.nextLine();
+		            Medicamento medicamento= new Medicamento(codigo, descripcion, precio, stock);
+		            gestionGuardia.registrarMedicamento(medicamento);
+	        	}
 	            break;
 	
-	        case 2:
-	            System.out.print("Ingrese la matrícula del médico: ");
-	            int matricula = input.nextInt();
-	            System.out.print("Ingrese el nombre del médico: ");
-	            String nombreMedico = input.nextLine();
-	            System.out.print("Ingrese la especialidad (cirujano o clínico): ");
-	            String especialidad = input.nextLine();
-	            Medico medico = new Medico(matricula, nombreMedico, especialidad);
-	            gestionGuardia.registrarMedico(medico);
-	            System.out.println("Médico registrado con éxito.");
-		        break;
+	        
 	    }
-	}
+	  }
 	}
 }
